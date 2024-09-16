e6f70b43ab98a3e5816e0d25579b6024599e23c9
ThomasVu <samduykhanh01@gmail.com>
2024-09-16 01:18:08 -0400

    Edit the Parser.java file
    Added the error function to Lox and Parser to return the error.
    Also, make ParseError extends RunTimeException to prevent the parser to
    produce false-positive error due to a side-effect of syntax error.

--- java/com/craftinginterpreters/lox/Parser.java
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,9 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {
+  }
+
   private final List<Token> tokens;
   private int current = 0;
 
@@ -161,5 +164,10 @@ class Parser {
 
   private Token previous() {
     return tokens.get(current - 1);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
   }
 }
